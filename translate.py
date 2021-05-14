e343eb874aa4883b5ee3f3aadace5d4404e9a6aa
razakgit <register.razak@gmail.com>
2021-05-14 21:25:34 +0530

    added proc,build pack

--- translate.py
+++ translate.py
@@ -1,7 +1,42 @@
-from googletrans import Translator
-translator = Translator()
 
-translatedText=translator.translate('ஒரு நல்ல வாய்ப்பு')
-print('TranslatedText:'+translatedText.text)
-print('Source Language:'+translatedText.src)
-print('Dest Language:'+translatedText.dest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from googletrans import Translator
+# from flask import Flask
+
+# my_awesome_app = Flask(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 )
+
+
+# @my_awesome_app.route('/')
+# def hello_world():
+#     translator = Translator()
+#     translatedText=translator.translate('ஒரு நல்ல வாய்ப்பு')
+#     print('TranslatedText:'+translatedText.text)
+#     print('Source Language:'+translatedText.src)
+#     print('Dest Language:'+translatedText.dest)
+
+
+# if __name__ == '__main__':
+#     my_awesome_app.run()
