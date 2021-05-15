59e0a24a01e855ee535ae67e2fdc59d959d2f2ac
razakgit <register.razak@gmail.com>
2021-05-15 14:49:05 +0530

    remove unwanted files

--- speectToText.py
+++
@@ -1,6 +0,0 @@
-import speech_recognition as sprec
-rec1=sprec.Recognizer()
-with sprec.Microphone() as source:
-    audioFromMic=rec1.listen(source)
-    convertedTextFromAudio=rec1.recognize_google(audioFromMic)
-    print(convertedTextFromAudio)
